1e3af31b9c93b0ea175ae4aee83a3fa33e2fdd08
Code-in-Website <chaejinseong4@gmail.com>
2025-02-21 22:58:55 +0900

    수정4

--- src/routes/openaiRouter.js
+++ src/routes/openaiRouter.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { getOpenAIResponse } from "../controllers/openaiController.js"; 
+import { summarizeTextController } from "../controllers/openaiController.js"; 
 
 const router = express.Router();
 
 // OpenAI API 라우트
-router.post("/chat", getOpenAIResponse);
+router.post("/summarize", summarizeTextController);
 
 export default router;
