44b26b4f9ad746c8a5001d2c82924910ad2b1623
xEzIxX <by1094@gachon.ac.kr>
2025-02-21 15:41:16 +0900

    feat: URL-encoded 데이터 파싱 활성화  - 폼 데이터 처리를 위한 미들웨어 적용

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(process.env.PORT, (err) => {
   console.log(`Server running on port ${process.env.PORT}`);
