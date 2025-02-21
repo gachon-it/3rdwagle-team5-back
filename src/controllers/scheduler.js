eec81e1627cc8d51954c881bb543d54a82cc8763
xEzIxX <by1094@gachon.ac.kr>
2025-02-22 00:32:37 +0900

    chore : 사용하지 않는 일정 등록 API 삭제 (프론트에서 담당하기로 함)

--- src/controllers/scheduler.js
+++ src/controllers/scheduler.js
@@ -7,7 +7,7 @@ export const scheduleRouter = Router();
 const upload = multer({ dest: "uploads/" });
 
 /**
- * 1️⃣ 일정 추출 API (OCR 분석)
+ * 일정 추출 API (OCR 분석)
  * - URL: `/schedule/upload`
  * - Method: POST
  * - Content-Type: multipart/form-data
@@ -31,35 +31,5 @@ scheduleRouter.post("/upload", upload.single("file"), async (req, res) => {
   } catch (error) {
     console.error("이미지 처리 에러 발생:", error);
     res.status(500).send({ success: false, error: "이미지 처리 실패" });
-  }
-});
-
-/**
- * 2️⃣ 일정 등록 API (구글 캘린더에 등록)
- * - URL: `/schedule/calendar`
- * - Method: POST
- * - Content-Type: application/send
- */
-scheduleRouter.post("/calendar", async (req, res) => {
-  console.log(req.body);
-  const { title, date, start_time, end_time, location, description } = req.body;
-
-  if (!title || !date || !start_time || !end_time) {
-    return res.status(400).send({ success: false, error: "요청 형식 오류" });
-  }
-
-  try {
-    // 구글 캘린더에 일정 등록하는 서비스 함수 호출
-
-    return res.status(200).send({
-      success: true,
-      message: "캘린터에 일정 등록 완료",
-    });
-  } catch (error) {
-    console.error("캘린더에 일정 등록 중 에러 발생:", error);
-    res.status(500).send({
-      success: false,
-      error: "캘린터에 일정 등록 실패",
-    });
   }
 });
